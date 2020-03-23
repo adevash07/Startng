a9b1beb7094b468ac0194500ee1fc54b86cce163
Adevash07 <adeyemiibrahim223@gmail.com>
2020-03-23 14:38:13 -0500

    js-test

---
+++ javascript/script.js
@@ -0,0 +1,18 @@
+var myName = "Adeyemi Ibrahim";
+
+var myCourses = ['Html', 'css', 'javascript','Node.js']
+
+document.getElementById("name").innerHTML ='<h1>' + myName; + '</h1>';
+document.getElementById("courses").innerHTML = " " + myCourses;
+console.log(myName);
+console.log(myCourses);
+
+if ((myCourses.length % 2)  == 0 ) {
+	var number = [];
+	number.length = 201;
+	for (var i = 0; i < number.length; i++) {
+		if ((i % 2) == 0) {
+			console.log(i);
+		}
+	}
+}
